41489461f169e917473c675b9e2067e180eca07d
RyanDur <ryan.p.durling@gmail.com>
2018-10-04 20:46:44 -0500

    ekoaerv

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebpackMonitor = require('webpack-monitor');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   devServer: {
     contentBase: './dist'
-  }
+  },
+  plugins: [
+    new WebpackMonitor({
+      capture: true,
+      target: '../monitor/myStatsStore.json',
+      launch: true,
+      port: 3030
+    })
+  ]
 });
