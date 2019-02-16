beb858c4c425e8b2f4ea46dff0174dbe2c67a7d5
RyanDur <ryan.p.durling@gmail.com>
2019-02-16 14:07:14 -0600

    asdgfaf

--- src/core/clientConnector.ts
+++ src/core/clientConnector.ts
@@ -2,9 +2,8 @@ import {ValidationError} from 'io-ts';
 import {failure} from 'io-ts/lib/PathReporter';
 import {Response, ValidateResponse} from './types/Response';
 
-export const clientConnector = (socket: WebSocket) => (fn: (data: Response) => void) => {
-  socket.onmessage = (data) => fn(ValidateResponse.decode(JSON.parse(data.data))
+export const clientConnector = (socket: WebSocket) => (fn: (data: Response) => void) =>
+  socket.onmessage = ({data}) => fn(ValidateResponse.decode(JSON.parse(data))
     .getOrElseL((errors: ValidationError[]) => {
       throw new Error(failure(errors).join('\n'));
     }) as Response);
-};
