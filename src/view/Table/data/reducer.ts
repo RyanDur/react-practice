1227bba65ca84e315a148daeb60ab488d6d3ed21
RyanDur <ryan.p.durling@gmail.com>
2019-03-11 20:06:13 -0500

    dsfsdf

--- src/view/Table/data/reducer.ts
+++ src/view/Table/data/reducer.ts
@@ -1,7 +1,7 @@
 import {remove} from '../../../util/ObjectHelpers';
 import {sumColumns} from '../../helpers';
 import {dataAction, DataAction} from './action';
-import {DataResponse, DataState, Table} from './types';
+import {Data, DataResponse, DataState, Table} from './types';
 import {ResponseData} from './types/DataResponse';
 
 const defaultState: DataState = {
@@ -32,9 +32,7 @@ const buildTable = ({data, columnNames}: DataResponse): Table =>
     data.reduce((table: Table, newRow: ResponseData) => {
       if (Object.keys(table).includes(newRow.name)) {
         const subRows = [...(table[newRow.name].subRows || [table[newRow.name].data]), remove(newRow, 'name')];
-        return {
-          ...table, [newRow.name]: {data: sumColumns(subRows, columnNames), subRows}
-        };
+        return {...table, [newRow.name]: {data: sumColumns(subRows, columnNames), subRows}};
       }
       return {...table, [newRow.name]: {data: remove(newRow, 'name')}};
     }, {}) : {};
