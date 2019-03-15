5fd294b1def3ad83b1efb00d0fa6fe03b70768be
RyanDur <ryan.p.durling@gmail.com>
2019-03-15 14:16:15 -0500

    asdgfx

--- src/view/Table/Selectable/state/selectors.ts
+++ src/view/Table/Selectable/state/selectors.ts
@@ -2,7 +2,9 @@ import {AppState} from '../../../../types';
 import {sumColumns} from '../../../helpers';
 
 export const selectedTotals = ({base, selectable, data}: AppState) =>
-  sumColumns(Object.keys(selectable.selected).map((row) => data.table[row].data), data.columns);
+  sumColumns(Object.entries(selectable.selected)
+    .filter(([_, selected]) => selected)
+    .map(([row]) => data.table[row].data), data.columns);
 
 export const selectableRows = ({base, selectable, data}: AppState) =>
   base.rowNames.map((row: string) => ({
