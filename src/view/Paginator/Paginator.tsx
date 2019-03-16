000bc309f9bc0f0dabeaee845bd6ff7b3ae057f0
RyanDur <ryan.p.durling@gmail.com>
2019-03-16 18:44:22 -0500

    sdafd

--- src/view/Paginator/Paginator.tsx
+++ src/view/Paginator/Paginator.tsx
@@ -1,7 +1,7 @@
 import {ReactNode} from 'react';
 import * as React from 'react';
-import './Paginator.css';
 import {PageDispatchProps, PageStateProps} from './state/connector';
+import './Paginator.css';
 
 interface ManualProps {
   children: ReactNode[];
@@ -13,12 +13,14 @@ export const Paginator = ({children, changePage, currentPage}: WindowProps) => {
   const active = (page: number) => page === currentPage;
   return <section className='paginator'>
     {children.map((child, page) => active(page) && child)}
-    {children.map((_, page) =>
-      <input key={page}
-             type='radio'
-             name={'paginator'}
-             value={page}
-             checked={active(page)}
-             onChange={() => changePage(page)}/>)}
+    <nav className='menu'>
+      {children.map((_, page) =>
+        <input key={page}
+               type='radio'
+               name={'paginator'}
+               value={page}
+               checked={active(page)}
+               onChange={() => changePage(page)}/>)}
+    </nav>
   </section>;
 };
